e302f386926a2122c27865b5e6eab9d0dcb9ee12
Zyambo <mightypz2@gmailcom>
2022-12-17 10:02:11 +0200

    change project layout

--- README.md
+++ README.md
@@ -29,4 +29,22 @@ Sqlite in developmeent and MySQL in production
 - Adds payments to DB automatically
 - Print reports
 
+## Run The Application
+Now you can run your application using the flask command. From the terminal, tell Flask where to find your application, then run it in debug mode. Remember, you should still be in the top-level directory, not the SKOOLPAY package.
+
+Debug mode shows an interactive debugger whenever a page raises an exception, and restarts the server whenever you make changes to the code. You can leave it running and just reload the browser page as you follow the tutorial.
+
+$ flask --app SKOOLPAY --debug run
+
+## Initialize the Database File
+You initialize the database by calling the init-db method that has been registered with the app, it can be called using the flask command, similar to the run command from the previous page.
+
+Run the init-db command:
+
+$ flask --app flaskr init-db
+
+Initialized the database.
+
+There will now be a skoolpay.sqlite file in the instance folder in your project.
+
 Auth: Zyambo <zyambo@icloud.com>
--- app.py
+++ app.py
@@ -1,64 +1,55 @@
-from flask import Flask, request, render_template, redirect, sessions
+# from flask import Flask, request, render_template, redirect, sessions
+# from flask import url_for
 
-app = Flask(__name__)
-# Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-# Ensure responses aren't cached
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
+# app = Flask(__name__)
+# # Ensure templates are auto-reloaded
+# app.config["TEMPLATES_AUTO_RELOAD"] = True
+# # Ensure responses aren't cached
+# @app.after_request
+# def after_request(response):
+#     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#     response.headers["Expires"] = 0
+#     response.headers["Pragma"] = "no-cache"
+#     return response
 
-@app.route('/', methods = ['GET', 'POST'])
-def homepage():
-    return render_template('school/index.html')
+# @app.route('/', methods = ['GET', 'POST'])
+# def homepage():
+#     return render_template('index.html')
 
 
-@app.route('/dashboard', methods = ['GET', 'POST'])
-def dashboard():
-    return render_template('school/dashboard.html', session="show")
-
-@app.route('/login', methods = ['GET', 'POST'])
-def login():
-    try:
-        if request.method =='GET':
-            return render_template('auth/login.html')
-        return redirect('/dashboard')
-    except Exception as e:
-        print(e)
+# @app.route('/dashboard/<user>', methods = ['GET', 'POST'])
+# def dashboard(user):
+#     return render_template('dashboard.html', session=user)
     
+    
+# @app.route('/register', methods = ['GET', 'POST'])
+# def register():
+#     try:
+#         if request.method == 'GET':
+#             return render_template('auth/register.html')
+#         return redirect('/login')
+#     except Exception as e:
+#         print(e)
 
+# @app.route('/signup', methods = ['GET', 'POST'])
+# def signup():
+#     try:
+#         if request.method =='GET':
+#             return render_template('auth/signup.html')
+#         return redirect('/login')
+#     except Exception as e:
+#         print(e)
 
-@app.route('/register', methods = ['GET', 'POST'])
-def register():
-    try:
-        if request.method =='GET':
-            return render_template('auth/register.html')
-        return redirect('/login')
-    except Exception as e:
-        print(e)
+# @app.route('/history', methods = ['GET', 'POST'])
+# def history():
+#     return render_template('history.html')
 
-@app.route('/signup', methods = ['GET', 'POST'])
-def signup():
-    try:
-        if request.method =='GET':
-            return render_template('auth/signup.html')
-        return redirect('/login')
-    except Exception as e:
-        print(e)
+# @app.route('/logout')
+# def logout():
+#     try:
+#         return redirect('/')
+#     except Exception as e:
+#         print(e)
 
-@app.route('/history', methods = ['GET', 'POST'])
-def history():
-    return render_template('history.html')
-
-@app.route('/logout')
-def logout():
-    try:
-        return redirect('/')
-    except Exception as e:
-        print(e)
-
-if __name__=='__main__':
-    app.run(port=5000, debug=True)
+# if __name__=='__main__':
+#     app.run(port=5000, debug=True)
